942894c210aec99539beeca9db971c4199fcf954
Mike Whaley <michael.d.whaley@gmail.com>
2023-02-21 22:26:47 -0500

    try pages

---
+++ index.js
@@ -0,0 +1,43 @@
+const nameInput = document.querySelector("#name-input");
+const emailInput = document.querySelector("#email-input");
+const startTime = document.querySelector("#startTime-input");
+const finishTime = document.querySelector("#finishTime-input");
+const category = document.querySelector("#category-input");
+const submitButton = document.querySelector("#submit");
+const returnData = document.querySelector("#return-data");
+const inputForm = document.querySelector("input-form");
+
+function formReset() {
+  nameInput.value = "";
+  emailInput.value = "";
+  startTime.value = "";
+  finishTime.value = "";
+  category.value = "Chestnut Ridge";
+}
+
+function hoursSubmit(e) {
+  e.preventDefault();
+  returnData.innerHTML = "";
+  let hours = (
+    (Date.parse(finishTime.value) - Date.parse(startTime.value)) /
+    3600000
+  ).toFixed(2);
+  let message = document.createElement("p");
+  if (nameInput.value === "") {
+    message.textContent = "Please enter your name.";
+  } else if (emailInput.value === "") {
+    message.textContent = "Please enter an email address.";
+  } else if (startTime.value === "") {
+    message.textContent = "Please enter a Start Time.";
+  } else if (finishTime.value === "") {
+    message.textContent = "Please enter a Finish Time.";
+  } else if (hours < 0) {
+    message.textContent = "Whoa! Did you invent time travel?";
+  } else {
+    message.textContent = `Thank You ${nameInput.value} for volunteering ${hours} hours at ${category.value}!`;
+  }
+  returnData.appendChild(message);
+  formReset();
+}
+
+submitButton.addEventListener("click", hoursSubmit);
